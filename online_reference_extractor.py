f6a947baefab3c553980045931f16858d512bc66
Jialong Zhu <jzhu09@qub.ac.uk>
2023-07-12 23:11:05 +0100

    add data preparation

--- online_reference_extractor.py
+++ online_reference_extractor.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 
 def pubmed(title):
@@ -17,9 +18,13 @@ def pubmed(title):
 
     summary_response = requests.get(summary_url).text
     summary_json = json.loads(summary_response)
-    summ = summary_json['result'][ids[0]]
 
-
+    print(summary_json)
+    try:
+        summ = summary_json['result'][ids[0]]
+    except KeyError:
+        print('cannot find the search result, continue to the next one!')
+        return ""
 
     # generate the customised dict
     pubmed_ref = {}
@@ -29,7 +34,7 @@ def pubmed(title):
         authors.append(author['name'])
     pubmed_ref['authors'] = authors
 
-    pubmed_ref['title'] = summ['title']
+    pubmed_ref['title'] = summ['sorttitle']
 
     pubmed_ref['volume'] = summ['volume']
 
@@ -37,8 +42,56 @@ def pubmed(title):
 
     pubmed_ref['pages'] = summ['pages']
 
+    pubmed_ref['journal'] = summ['source']
+
+    pubmed_ref['year'] = summ['pubdate'][:4]
+
     for idtype in summ['articleids']:
         if idtype['idtype'] == 'doi':
             pubmed_ref['doi'] = idtype['value']
 
     return pubmed_ref
+
+
+def get_ref(title):
+    curr_ref = []
+    res = pubmed(str(title))
+    curr_ref.append((', '.join(res['authors']), 'authors'))
+    curr_ref.append((res['title'], 'title'))
+    curr_ref.append((res['volume'], 'volume'))
+    curr_ref.append((res['issue'], 'issue'))
+    curr_ref.append((res['pages'], 'pages'))
+    curr_ref.append((res['journal'], 'journal'))
+    curr_ref.append((res['year'], 'year'))
+    try:
+        curr_ref.append((res['doi'], 'doi'))
+    except:
+        curr_ref.append(('', 'doi'))
+    return curr_ref
+
+
+if __name__ == '__main__':
+    ref_tags = []
+    for i in range(200):
+        curr_ref = []
+        res = pubmed(str(i))
+        curr_ref.append((', '.join(res['authors']), 'authors'))
+        curr_ref.append((res['title'], 'title'))
+        curr_ref.append((res['volume'], 'volume'))
+        curr_ref.append((res['issue'], 'issue'))
+        curr_ref.append((res['pages'], 'pages'))
+        curr_ref.append((res['journal'], 'journal'))
+        curr_ref.append((res['year'], 'year'))
+        try:
+            curr_ref.append((res['doi'], 'doi'))
+        except:
+            curr_ref.append(('', 'doi'))
+        ref_tags.append(curr_ref)
+        time.sleep(1)
+        print(i)
+
+    with open('train.txt', 'a') as file:
+        # Write each element of the list on a separate line
+        for row in ref_tags:
+            line = repr(row)
+            file.write(line + '\n')
